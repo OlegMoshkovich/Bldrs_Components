c41ff667af0ebab55481503a7eed64ea914ba1fc
OlegMoshkovich <oleg.mosh@gmail.com>
2023-08-19 09:24:50 +0200

    minor

--- src/SideDrawer.jsx
+++ src/SideDrawer.jsx
@@ -54,7 +54,7 @@ export default function TemporaryDrawer() {
                 variant={'contained'}
                 onClick={toggleDrawer}
               >
-                Close drawer
+                Close
               </Button>
             </Stack>
             {isNotes &&

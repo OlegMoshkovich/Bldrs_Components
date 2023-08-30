ed62404a88ccba366919d2ea0845dd2ec3366bbe
OlegMoshkovich <oleg.mosh@gmail.com>
2023-08-30 16:59:26 +0200

    turn the coponent library on

--- src/Store.js
+++ src/Store.js
@@ -3,7 +3,7 @@ import { create } from 'zustand'
 const useStore = create((set) => ({
   borderRadius: 5,
   themeScheme: 0,
-  showComponents: false,
+  showComponents: true,
   setBorderRadius: (value) => set((state) => ({ borderRadius: value})),
   setThemeScheme: (value) => set((state) => ({ themeScheme: value})),
   toggleShowComponents: (value) => set((state) => ({ showComponents: !state.showComponents})),

541e8a646122c54213204807d2218ca2c24a558c
OlegMoshkovich <oleg.mosh@gmail.com>
2023-09-06 11:08:24 +0200

    remove the borde from the app bar

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ const themeComponent = {
       styleOverrides: {
         root: ({ theme }) => ({
           backgroundColor: theme.palette.background.default, // use palette color here
-          borderBottom:`1px solid ${theme.palette.secondary.main}`
+          // borderBottom:`1px solid ${theme.palette.secondary.main}`
         }),
       },
     },

a9d8d67d088e6ee140db096a6d481b77fedda0b8
OlegMoshkovich <oleg.mosh@gmail.com>
2023-09-28 17:44:37 +0200

    work on the note narrative

--- src/Store.js
+++ src/Store.js
@@ -3,12 +3,14 @@ const firstNote =
 {
   title:'Welcome to Notes',
   date: '17.09.2023',
-  content:`We believe that attaching notes to content is valuable.
-  When choosing integration tools, it's crucial to find the right fit. Notion, Linear,
-  and GitHub are solid choices for note-based collaboration. While GitHub is foundational and linked to Share by default,
-  other platforms might better suit your needs. Just connect to your preferred solution.`,
+  content:`We believe attaching notes to 3D content is a valuable addition to the standard collaboration workflows.
+  We believe in using the right tool for the job, and as an integration enviromemnt,
+  offer several integrations to enable note-based collaboration.
+  When choosing the tools, it's crucial to find the right fit. Notion, Linear,
+  and GitHub are solid choices for note-based worlflows. While GitHub is foundational to our ecosystem and linked to Share by default,
+  other platforms might better suit your needs. Just connect to your preferred solution. Visit our Wiki to learn more about integrations`,
   type:'image',
-  src:'https://i.pinimg.com/564x/ac/6d/50/ac6d50ccbde053337dae0390bd44a193.jpg'
+  src:'https://user-images.githubusercontent.com/3433606/271329214-bfde5dde-184c-4796-ae7c-fb2b2ab8266c.png'
 }
 
 const useStore = create((set) => ({
